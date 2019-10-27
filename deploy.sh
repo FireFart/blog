de6bd95ab73e82145735b52c0fb08ecdd7aadc51
Christian Mehlmauer <firefart@gmail.com>
2019-10-27 18:45:51 +0100

    more work

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 ./hugo
 cd public
+git checkout master
 git add .
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
@@ -13,8 +14,3 @@ git commit -m "$msg"
 git push origin master
 # echo checking links ...
 #bundle exec htmlproofer ./public
-#echo syncing stuff...
-#rsync --progress -arzvh -e "ssh -p 9988" --delete-before --delete public/ root@firefart.at:/var/www/blog/
-#echo chmodding all the things...
-#ssh -p 9988 root@firefart.at "chown -R www-data:www-data /var/www/blog/"
-#echo finished
